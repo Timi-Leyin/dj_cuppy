bee54708f05a1bc7f0ebf346958e6455d98bdb11
Timileyin <timileyinoyelekan11@gmail.com>
2021-01-24 05:39:46 +0000

    Completed

--- script.js
+++ script.js
@@ -11,6 +11,47 @@ function window_loaded(){
  preloader.classList.add('active');
 main_wrapper.classList.add('active');
 }
+// follow
+const follow_btn=document.querySelector('.follow-btn');
 
+
+follow_btn.addEventListener('click',function(){
+    follow_btn.classList.toggle('active');
+ 
+if(follow_btn.className=='follow-btn btn active'){
+    follow_btn.innerText='Following'
+}else{
+    follow_btn.innerText='Follow'
+
+}
+})
+
+
+const pop_up=document.querySelector('.popup');
+const pop_up_bg=document.querySelector('.pop-up-bg');
+const close_pop_up=document.querySelector('.popup .close');
+const ellipsis=document.querySelector('.--icon-s');
+
+function OpenPopUp(){
+pop_up.classList.add('active');
+pop_up_bg.classList.toString('active')
+}
+function ClosePopUp(){
+pop_up.classList.remove('active');
+}
+
+ellipsis.onclick=()=>{
+pop_up.classList.toggle('active');
+pop_up_bg.classList.toggle('active')
+
+}
+close_pop_up.onclick=()=>{
+    pop_up.classList.toggle('active');
+pop_up_bg.classList.toggle('active')
+}
+pop_up_bg.onclick=()=>{
+    pop_up.classList.toggle('active');
+pop_up_bg.classList.toggle('active')
+}
 
 })(window)
